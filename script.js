f5482a879c67898f74a6ac5ea64dc287fdc56109
Надежда Чеплагина <makidaschka88@gmail.com>
2021-02-24 17:05:44 +0400

    клики по картам

--- script.js
+++ script.js
@@ -1,58 +1,113 @@
-let easyLvl = document.getElementById('easyLevel');
-let normalLvl = document.getElementById('normalLevel');
-let hardLvl = document.getElementById('hardLevel');
-let easyLvlCheck = document.getElementById('easyCheck');
-let normalLvlCheck = document.getElementById('normalCheck');
-let hardLvlCheck = document.getElementById('hardCheck');
 let displayMenu = document.getElementById('displayMenu');
 let button = document.getElementById('button');
+let quantityCards = 3;
+let click = 0;
+let level = document.querySelector('.start__level').getAttribute('id');
+let randomI = 0;
+let arr = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-    easyLvlCheck.classList.add('menu__start__rhomb');
-	normalLvlCheck.classList.remove('menu__start__rhomb');
-	hardLvlCheck.classList.remove('menu__start__rhomb');
-	displayMenu.classList.remove('displayNone');
-  });
-
-easyLvl.addEventListener( 'click', () => {
-	easyLvl.classList.add('selected');
-	normalLvl.classList.remove('selected');
-	hardLvl.classList.remove('selected');
-	easyLvlCheck.classList.add('menu__start__rhomb');
-	normalLvlCheck.classList.remove('menu__start__rhomb');
-	hardLvlCheck.classList.remove('menu__start__rhomb');
+document.querySelectorAll('li.start__level').forEach(e => {
+	e.addEventListener('click', levelClick)
 })
 
-normalLvl.addEventListener( 'click', () => {
-	normalLvl.classList.add('selected');
-	easyLvl.classList.remove('selected');
-	hardLvl.classList.remove('selected');
-	easyLvlCheck.classList.remove('menu__start__rhomb');
-	normalLvlCheck.classList.add('menu__start__rhomb');
-	hardLvlCheck.classList.remove('menu__start__rhomb');
-})
+ function levelClick(e) {
+ 	level = e.target.getAttribute('id')
+		switch (level) {
+			case 'easyLevel':
+			quantityCards = 3;
+			break;
+			case 'normalLevel':
+			quantityCards = 6;
+			break;
+			case 'hardLevel':
+			quantityCards = 10;
+			break;
+		}
+ 	document.getElementById('easyLevel').classList.remove('selected', 'red');
+ 	document.getElementById('normalLevel').classList.remove('selected', 'red');
+ 	document.getElementById('hardLevel').classList.remove('selected', 'red');
+ 	e.target.classList.add('selected','red');
+ }
 
-hardLvl.addEventListener( 'click', () => {
-	hardLvl.classList.add('selected');
-	normalLvl.classList.remove('selected');
-	easyLvl.classList.remove('selected');
-	easyLvlCheck.classList.remove('menu__start__rhomb');
-	normalLvlCheck.classList.remove('menu__start__rhomb');
-	hardLvlCheck.classList.add('menu__start__rhomb');
-})
-let hard = hardLvl.addEventListener();
+ function createCard() {
+	let newCard = document.createElement('div');
+	newCard.className = 'game__card'
+	document.querySelector('.game').append(newCard)
+	if( quantityCards > 3){
+			newCard.classList.add('game__hard__card')
+		} else{
+			newCard.classList.remove('game__hard__card')
+		}
+	newCard.addEventListener('click', rotateCard) 
+		/*newCard.addEventListener('click', function() {
+		for (i = 0; i < 3; i++){
+			click++;
+			console.log(click);
 
-button.addEventListener( 'click', () => {
+			if(click < 2) {
+				rotateCard();
+			} else {
+				goToMenu();
+			}}
+		})*/
+	};
+	
+ function rotateCard(e) {
+		console.log(arr[randomI]);
+	if(e.target === arr[randomI]){
+	arr[randomI].classList.add('rotate__bag__card');
+}else {
+	e.target.classList.add('rotate__card')
+}
+}
+
+button.onclick = () => {
 	displayMenu.classList.add('displayNone');
-	var section = document.createElement('section');
+	let section = document.createElement('section');
 	section.className = 'game';
 	document.body.append(section);
-	var div = document.createElement('div');
-	div.className = 'game__cards';
-	document.body.append(div);
-	div.onclick = function () {
-	div.style.background = "url(img/end.png)";
-	div.style.transform = "none";
+	if( quantityCards === 10){
+			document.querySelector('.game').classList.add('game__hard')
+		} else{
+			document.querySelector('.game').classList.remove('game__hard')
+	}
+	randomI = Math.floor(Math.random() * (quantityCards-1));
+	console.log(randomI)
+		for (let i = 0; i < quantityCards; i++) {
+		
+		createCard();
+		if (i === arr[randomI]) {
+	arr[randomI].classList.remove('rotate__card');
+    arr[randomI].classList.add('rotate__bag__card');
+			}
+		}
+	arr = document.querySelectorAll('.game__card');
+	console.log(arr);
+	console.log(arr[randomI]);
 }
-})
 
+/*document.querySelectorAll('.game__card').forEach(e => {
+	console.log(e);
+	e.addEventListener('click', (goToMenu) => {
+		e.onclick = function() {
+		click++;
+		console.log(click);
+	}
+})})*/
+
+let goToMenu = function () {
+	displayMenu.classList.remove('displayNone');
+	document.querySelector('.game').classList.add('displayNone')
+}
+
+/*newCard.onclick = () => {
+			for (i = 0; i < )
+			click++;
+			console.log(click);
+
+			if(click < 3) {
+				rotateCard();
+			} else {
+				goToMenu();
+			}}*/
+			//rotateCard();*/*/
